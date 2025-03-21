dafab78e1b7bc6e9f9949897324c96af3c83a673
[PROD] Mein-Chatserver <prod@mein-chatserver.de>
2025-03-21 12:49:54 +0100

    Rename generator

--- GenerateDocs.sh
+++ GenerateDocs.sh
@@ -2,18 +2,46 @@
 set -efu
 set -o errexit
 
-if [ "$(which phpDocumentor)" = "" ]; then
-	echo "phpDocumentor not installed!"
-fi
+{ # this ensures the entire script is downloaded #
 
-dir=$(pwd)
+# METHODS #
+	color() {
+		echo -e "$1\e[39m\033[0;37m\e[0m"
+	}
+	
+	error() {
+		color "\n\e[91m\033[41m\e[K"
+		color "\e[1;37m\033[41m\e[K  ERROR"
+		color "\e[1;37m\033[41m\e[K  $1\e[K"
+		color "\e[91m\033[41m\e[K \n"
+	}
+	
+	has() {
+		type "$1" > /dev/null 2>&1
+	}
+	
+# VARIABLES #
+	AUTO_UPLOAD=1
+	PHPDOC="$(which phpDocumentor)"	
+	WORKING_DIR=$(pwd)
 
-echo "Working-Path: $dir"
+# HANDLING #
+	if [ $PHPDOC = "" ]; then
+		error "Can't find phpDocumentor!\n  Please install phpDocumentor first."
+	else
+		color "\e[32m[OK]\e[39m Found phpDocumentor on: $PHPDOC"
+		color "\e[32m[OK]\e[39m Working on Directory: $WORKING_DIR"
+		
+		# Generate the Docs
+		php $PHPDOC run --template="$WORKING_DIR/Docs/theme" --cache-folder="$WORKING_DIR/tmp" --directory="$WORKING_DIR" --target="$WORKING_DIR/tmp/Docs"
 
-# Generate the Docs
-php /usr/local/bin/phpDocumentor run --template="/opt/Website/Libraries/AlfaDNS/Docs/theme" --cache-folder="/tmp" --directory="/opt/Website/Libraries/AlfaDNS/" --target="/opt/Website/Libraries/AlfaDNS/Docs"
-
-# Remove unwanted Files
-mv ./Docs/classes/AlfaDNS/AlfaDNS.md ./Docs/README.md
-rm -rf ./Docs/classes
-rm -rf ./index.md
+		# Move Docs
+		[ -f "$WORKING_DIR/tmp/Docs/classes/AlfaDNS/AlfaDNS.md" ] && (mv "$WORKING_DIR/tmp/Docs/classes/AlfaDNS/AlfaDNS.md" "$WORKING_DIR/Docs/README.md")
+		
+		# Update Git
+		if [ $AUTO_UPLOAD = 1 ]; then
+			color "\e[32m[OK]\e[39m Uploading to Git"
+			git add . && git commit -m "Updating Docs" & git push
+		fi
+	fi
+} # this ensures the entire script is downloaded #
